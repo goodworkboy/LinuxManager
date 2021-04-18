31dbfe2843d64502438efe0e5ee1ce40c7194355
goodworkboy <liugxwy163@163.com>
2021-04-18 10:37:51 +0800

    Add execl resolver and test it

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # LinuxManager
-毕设（基于Docker的Linux实验管理平台的设计和实现
+毕设（基于Docker的Linux实验管理平台的设计
+#environment
+centos7
+python3.8.9
+docker 20.10.9
+
+#import execl
+url  https://blog.csdn.net/weixin_30064143/article/details/111968084
--- main.py
+++ main.py
@@ -3,6 +3,17 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+import xlrd
+
+url = "/home/liugx/test.xlsx"
+def test_xlrd():
+    data = xlrd.open_workbook(url)
+    table = data.sheet_by_index(0)
+    nrows = table.nrows
+    for i in range(1, nrows):
+        print(table.row_values(i))
+
+
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -12,5 +23,6 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+    test_xlrd()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
