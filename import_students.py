168fa20b93cc469f561f26b1e26c86ca3f24ece0
goodworkboy <liugxwy163@163.com>
2021-05-09 17:16:21 +0800

    添加注释，修改部分流程

--- import_students.py
+++ import_students.py
@@ -7,6 +7,7 @@ import global_variable as gl
 student_username_prefix = "jlurj"
 student_usergroup="students"
 
+#导入学生名单
 def action(path):
     try:
         data = xlrd.open_workbook(path)
@@ -19,7 +20,7 @@ def action(path):
         index = first_row.index("学号")
         studentNum1 = table.col_values(index, 1, nrows)
         studentNums = [str(int(studentNum1[i])) for i in range(0, len(studentNum1))]
-        gl.set_value("studentNums",studentNums)
+        gl.append_value("studentNums",studentNums)
         create_users(studentNums)
         backup()
     except FileNotFoundError:
@@ -28,11 +29,15 @@ def action(path):
         if isContinue == "y":
             importStudentsUI()
     except ValueError:
-        print("没有找到学生学号这一列")
+        print("没有找到’学号‘这一列")
+        isContinue = input("是否继续(y/n)?")
+        if isContinue == "y":
+            importStudentsUI()
 
-
+#创建实验账户的函数
 def create_users(studentNums):
-    passwd = "IZqjXY5YZu.9I"
+    passwd = "IZqjXY5YZu.9I" #初始密码‘12345678’
+    #为每一位学生创建实验账户
     for studentNum in studentNums:
         temp = os.system(gl.docker_command_pre+"useradd jlurj%s -p %s -g %s" % (studentNum , passwd,student_usergroup))
         if temp==0:
@@ -42,7 +47,7 @@ def create_users(studentNums):
             command = gl.docker_command_pre+"ln -s /src/experiences /home/"+student_username_prefix+studentNum+"/experiences"
             print("学号%s的实验帐户创建成功，帐户名为jlurj%s" % (studentNum, studentNum))
         else:
-            print("帐户创建失败，该学生%s帐户以存在"%(studentNum))
+            print("帐户创建失败，该学生%s实验帐户已存在"%(studentNum))
 
 
 def importStudentsUI():
@@ -52,6 +57,7 @@ def importStudentsUI():
     if choose == "y":
         action(path)
 
+#将创建好的实验账户名单保存到本地
 def backup():
     student_nums = gl.get_value(gl.gl_student_nums_key)
     fp = open(gl.system_data_file, "w", encoding="utf-8")
--- publish_question.py
+++ publish_question.py
@@ -3,6 +3,7 @@ import global_variable as gl
 
 docker_volume_mount_dir = "/src/experiences"
 
+#将实验题目拷贝到容器内实验目录在服务器上的挂载目录下#将实验题目拷贝到容器内实验目录在服务器上的挂载目录下
 def publishQuestionsUI1():
     studentNums = gl.get_value(gl.gl_student_nums_key)
     path = input("请输入实验题目路径：")
@@ -18,3 +19,6 @@ def publishQuestionsUI():
     result = os.system("cp -nR %s %s" %(path,docker_volume_mount_dir))
     if result > 0:
         print("题目发放失败")
+        isContinue = input("是否继续(y/n)?")
+        if isContinue == "y":
+            publishQuestionsUI()
